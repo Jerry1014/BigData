8fea8a80247010f9fc25b9aae4ff82afb2cda45d
Jerry <757320383@qq.com>
2019-07-16 13:24:51 +0800

    Initial commit

---
+++ src/main/java/cn/neu/hadoop/bigdata/BigdataApplication.java
@@ -0,0 +1,13 @@
+package cn.neu.hadoop.bigdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigdataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigdataApplication.class, args);
+    }
+
+}
