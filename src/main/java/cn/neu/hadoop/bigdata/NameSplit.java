6315571ef7d01a27049b7eeb7738b4a09aa0779f
Jerry <757320383@qq.com>
2019-07-17 23:09:31 +0800

    添加了角色名字字典，增加名字识别成功率

--- src/main/java/cn/neu/hadoop/bigdata/NameSplit.java
+++ src/main/java/cn/neu/hadoop/bigdata/NameSplit.java
@@ -3,6 +3,7 @@ package cn.neu.hadoop.bigdata;
 import lombok.extern.slf4j.Slf4j;
 import org.ansj.domain.Result;
 import org.ansj.domain.Term;
+import org.ansj.library.DicLibrary;
 import org.ansj.recognition.impl.NatureRecognition;
 import org.ansj.splitWord.analysis.ToAnalysis;
 import org.apache.hadoop.fs.Path;
@@ -13,19 +14,22 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.List;
 
 @Component
 @Slf4j
 public class NameSplit {
+    private static HashSet<String> name_set = new HashSet<>();
     private static String input_path = "/test/input";
     private static String output_path = "/test/output1";
 
-    public static class NameLineMapper extends Mapper<Object, Text, LongWritable, Text> {
+    public static class NameLineMapper extends Mapper<Object, Text, Text, Text> {
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             Result tem = ToAnalysis.parse(value.toString());
             new NatureRecognition().recognition(tem);
@@ -33,14 +37,18 @@ public class NameSplit {
 
             for (Term i : terms) {
                 if (i.getNatureStr().equals("nr")) {
-                    context.write((LongWritable) key, new Text(i.getName()));
+                    if (name_set.contains(i.getName())) {
+                        long line_num = ((LongWritable) key).get();
+                        context.write(new Text(((FileSplit) context.getInputSplit()).getPath().getName()
+                                + line_num), new Text(i.getName()));
+                    }
                 }
             }
         }
     }
 
-    public static class NameLineReducer extends Reducer<LongWritable, Text, NullWritable, Text> {
-        public void reduce(LongWritable key, Iterable<Text> values, Context context)
+    public static class NameLineReducer extends Reducer<Text, Text, NullWritable, Text> {
+        public void reduce(Text key, Iterable<Text> values, Context context)
                 throws IOException, InterruptedException {
             StringBuilder name_list = new StringBuilder();
             for (Text i : values) {
@@ -51,11 +59,19 @@ public class NameSplit {
         }
     }
 
-    public static void main(String name_node) throws IOException, InterruptedException, ClassNotFoundException {
+    public static void main(String name_node, String all_person_name) throws IOException, InterruptedException, ClassNotFoundException {
+        // 构建所有人物的字典
+        for (String i : all_person_name.split("\r\n")) {
+            name_set.add(i);
+            DicLibrary.insert(DicLibrary.DEFAULT, i, "nr", DicLibrary.DEFAULT_FREQ);
+        }
+
         Job job = Job.getInstance();
         job.setJarByClass(NameSplit.class);
         job.setMapperClass(NameLineMapper.class);
         job.setReducerClass(NameLineReducer.class);
+        // 哭了，这有个大坑，map的输出未指定是，会根据上一个即inputform的来，所以出现了LongWrite不匹配的问题
+        job.setMapOutputKeyClass(Text.class);
         job.setOutputKeyClass(LongWritable.class);
         job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(1);//设置reduce的个数
@@ -64,9 +80,9 @@ public class NameSplit {
         job.waitForCompletion(true);
     }
 
-    public static void main(String in_path, String out_path, String name_node) throws IOException, InterruptedException, ClassNotFoundException {
+    public static void main(String in_path, String out_path, String name_node, String all_person_name) throws IOException, InterruptedException, ClassNotFoundException {
         input_path = in_path;
         output_path = out_path;
-        main(name_node);
+        main(name_node, all_person_name);
     }
 }
--- src/test/java/cn/neu/hadoop/bigdata/CharacterRelationshipAnalysis.java
+++ src/test/java/cn/neu/hadoop/bigdata/CharacterRelationshipAnalysis.java
@@ -26,19 +26,24 @@ public class CharacterRelationshipAnalysis {
     public void all_mission_test() {
         // 任务初始化
         int cur_mission = 0;
+        String source_all_person_name_file_path = "C:\\tem\\jinyong_all_person.txt";
+        String all_person_name_file_path = "/test/userdic/";
+        String all_person_filename = "jinyong_all_person.txt";
         String source_data_path = "C:\\tem\\data";
         String input_path = namespace + "/input";
         String output_path_format = namespace + "/output";
 
         try {
-            cur_mission = 4;
+            cur_mission = 1;
 
 //            clear_output_directory(input_path);
 //            hadoopTemplate.uploadDir(source_data_path, input_path);
 //            cur_mission++;
-//            clear_output_directory(output_path_format + cur_mission);
-//            NameSplit.main(input_path, output_path_format + cur_mission, name_node);
-//            cur_mission++;
+            clear_output_directory(output_path_format + cur_mission);
+//            hadoopTemplate.uploadFile(source_all_person_name_file_path, all_person_name_file_path);
+            String all_person_name = hadoopTemplate.read(true, all_person_name_file_path + all_person_filename);
+            NameSplit.main(input_path, output_path_format + cur_mission, name_node, all_person_name);
+            cur_mission++;
 //            clear_output_directory(output_path_format + cur_mission);
 //            NameCount.main(output_path_format + (cur_mission - 1), output_path_format + cur_mission, name_node);
 //            cur_mission++;
@@ -50,8 +55,8 @@ public class CharacterRelationshipAnalysis {
 //            clear_output_directory(output_path_format + cur_mission);
 //            clear_output_directory(namespace + "/tmp");
 //            PageRankCompute.main(output_path_format + (cur_mission - 1), output_path_format + cur_mission, 1, name_node);
-            cur_mission++;
-            hadoopTemplate.read(output_path_format + (cur_mission - 1) + "/part-r-00000");
+//            cur_mission++;
+            hadoopTemplate.read(false, output_path_format + (cur_mission - 1) + "/part-r-00000");
         } catch (Exception e) {
             e.printStackTrace();
             assert false;
