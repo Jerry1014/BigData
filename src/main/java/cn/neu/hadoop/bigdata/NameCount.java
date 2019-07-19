2c73764cf8873eb5c0958c6194e25b2116a59b7e
Jerry <757320383@qq.com>
2019-07-19 14:20:42 +0800

    添加人名计数mapreduce

--- src/main/java/cn/neu/hadoop/bigdata/NameCount.java
+++ src/main/java/cn/neu/hadoop/bigdata/NameCount.java
@@ -3,12 +3,14 @@ package cn.neu.hadoop.bigdata;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.omg.CORBA.OBJ_ADAPTER;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -16,28 +18,17 @@ import java.io.IOException;
 @Component
 @Slf4j
 public class NameCount {
-    private static String input_path = "/test/input";
-    private static String output_path = "/test/output2";
-
-    public static class NameCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+    public class NameCountMapper extends Mapper<Object, Text, Text, IntWritable> {
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            String[] all_name = value.toString().split(" ");
-            for (int i = 0; i < all_name.length; i++) {
-                String first_person = all_name[i];
-                for (int j = i + 1; j < all_name.length; j++) {
-                    String second_person = all_name[j];
-                    if (first_person.equals(second_person)) continue;
-                    if (first_person.compareTo(second_person) > 0)
-                        context.write(new Text(first_person + ',' + second_person), new IntWritable(1));
-                    else context.write(new Text(second_person + ',' + first_person), new IntWritable(1));
-                }
+            String[] names = value.toString().split(" ");
+            for (String i : names) {
+                context.write(new Text(i), new IntWritable(1));
             }
         }
     }
 
-    public static class NameCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-        public void reduce(Text key, Iterable<IntWritable> values, Context context)
-                throws IOException, InterruptedException {
+    public class NameCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable i : values) {
                 sum += i.get();
@@ -46,23 +37,17 @@ public class NameCount {
         }
     }
 
-    public static void main(String name_node) throws IOException, InterruptedException, ClassNotFoundException {
+    public void main(String in_path, String out_path, String name_node) throws IOException, ClassNotFoundException, InterruptedException {
         Job job = Job.getInstance();
         job.setJarByClass(NameCount.class);
         job.setMapperClass(NameCountMapper.class);
-        job.setCombinerClass(NameCount.NameCountReducer.class);
-        job.setReducerClass(NameCount.NameCountReducer.class);
+        job.setCombinerClass(NameCountReduce.class);
+        job.setReducerClass(NameCountReduce.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        job.setNumReduceTasks(1);
-        FileInputFormat.addInputPath(job, new Path(name_node + input_path));
-        FileOutputFormat.setOutputPath(job, new Path(name_node + output_path));
+        job.setNumReduceTasks(1);//设置reduce的个数
+        FileInputFormat.addInputPath(job, new Path(name_node + in_path));
+        FileOutputFormat.setOutputPath(job, new Path(name_node + out_path));
         job.waitForCompletion(true);
-    }
-
-    public static void main(String in_path, String out_path, String name_node) throws IOException, InterruptedException, ClassNotFoundException {
-        input_path = in_path;
-        output_path = out_path;
-        main(name_node);
     }
 }
