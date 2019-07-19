e021bccbc8e58a0b87817a7507144686183605c0
Jerry <757320383@qq.com>
2019-07-19 09:57:54 +0800

    将聚类的权重值换为根据自身的出边决定，效果。。。好像差不多

--- src/main/java/cn/neu/hadoop/bigdata/LPA/LPACompute.java
+++ src/main/java/cn/neu/hadoop/bigdata/LPA/LPACompute.java
@@ -40,9 +40,9 @@ public class LPACompute {
             for (String i : chain_name_and_weight_list) {
                 String[] name_and_weight = i.split(":");
 //                context.write(new Text(name_and_weight[0]), new Text(name_and_label[0] + '#' +
-//                        name_and_label[1] + '#' + name_and_weight[1]));
+//                        name_and_label[1] + '#' + PR * Float.valueOf(name_and_weight[1])));
                 context.write(new Text(name_and_weight[0]), new Text(name_and_label[0] + '#' +
-                        name_and_label[1] + '#' + PR * Float.valueOf(name_and_weight[1])));
+                        name_and_label[1]));
             }
         }
     }
@@ -51,33 +51,68 @@ public class LPACompute {
         HashMap<String, String> name_label = new HashMap<>();
 
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-            String PR_name_list = "";
-            Float max_weight = 0f;
-            List<String> max_weight_label = new LinkedList<>();
-            HashMap<String, Float> label_weight_sum = new HashMap<>();
-            for (Text i : values) {
-                String for_i_str = i.toString();
-                if (for_i_str.startsWith("$")) {
-                    PR_name_list = for_i_str.substring(1);
-                } else {
-                    String[] name_label_weight = for_i_str.split("#");
-                    if (name_label.containsKey(name_label_weight[0])) {
-                        name_label_weight[1] = name_label.get(name_label_weight[0]);
-                    }
-                    Float weight = 0f;
-                    if (label_weight_sum.containsKey(name_label_weight[1])) {
-                        weight = label_weight_sum.get(name_label_weight[1]);
-                    }
-                    weight += Float.valueOf(name_label_weight[2]);
-                    label_weight_sum.put(name_label_weight[1], weight);
+            // 这是根据对方与我的关系权重进行权重的相加
+            // 相应的map输出为<name, name2#name2label#name2->name.weight>
+//            String PR_name_list = "";
+//            Float max_weight = 0f;
+//            List<String> max_weight_label = new LinkedList<>();
+//            HashMap<String, Float> label_weight_sum = new HashMap<>();
+//            for (Text i : values) {
+//                String for_i_str = i.toString();
+//                if (for_i_str.startsWith("$")) {
+//                    PR_name_list = for_i_str.substring(1);
+//                } else {
+//                    String[] name_label_weight = for_i_str.split("#");
+//                    if (name_label.containsKey(name_label_weight[0])) {
+//                        name_label_weight[1] = name_label.get(name_label_weight[0]);
+//                    }
+//                    Float weight = 0f;
+//                    if (label_weight_sum.containsKey(name_label_weight[1])) {
+//                        weight = label_weight_sum.get(name_label_weight[1]);
+//                    }
+//                    weight += Float.valueOf(name_label_weight[2]);
+//                    label_weight_sum.put(name_label_weight[1], weight);
+//
+//                    if (weight > max_weight) {
+//                        max_weight = weight;
+//                        max_weight_label.clear();
+//                        max_weight_label.add(name_label_weight[1]);
+//                    } else if (weight.equals(max_weight)) {
+//                        max_weight_label.add(name_label_weight[1]);
+//                    }
+//                }
+//            }
+//
 
-                    if (weight > max_weight) {
-                        max_weight = weight;
-                        max_weight_label.clear();
-                        max_weight_label.add(name_label_weight[1]);
-                    } else if (weight.equals(max_weight)) {
-                        max_weight_label.add(name_label_weight[1]);
-                    }
+            List<String> label_buffer = new LinkedList<>();
+            String PR_name_list = "";
+            for (Text i : values) {
+                String tem = i.toString();
+                if (tem.startsWith("$")) {
+                    PR_name_list = tem.substring(1);
+                } else label_buffer.add(tem);
+            }
+
+            HashMap<String, Float> name_weight = new HashMap<>();
+            for (String i : PR_name_list.split("#")[1].split(";")) {
+                name_weight.put(i.split(":")[0], Float.valueOf(i.split(":")[1]));
+            }
+
+            HashMap<String, Float> label_point = new HashMap<>();
+            Float max_point = 0f;
+            List<String> max_weight_label = new LinkedList<>();
+            for (String i : label_buffer) {
+                String[] name2_label2 = i.split("#");
+                if (name_label.containsKey(name2_label2[0])) name2_label2[1] = name_label.get(name2_label2[0]);
+                Float weight = name_weight.get(name2_label2[0]);
+                if (label_point.containsKey(name2_label2[1])) weight += label_point.get(name2_label2[1]);
+                label_point.put(name2_label2[1], weight);
+                if (weight > max_point) {
+                    max_point = weight;
+                    max_weight_label.clear();
+                    max_weight_label.add(name2_label2[1]);
+                } else if (weight.equals(max_point)) {
+                    max_weight_label.add(name2_label2[1]);
                 }
             }
 
@@ -103,7 +138,7 @@ public class LPACompute {
             FileOutputFormat.setOutputPath(job, new Path(name_node + tmp_output_path + tmp_count));
             job.waitForCompletion(true);
 
-//            LPAResultClustering.main(name_node + tmp_output_path + tmp_count, name_node + tmp_output_path + tmp_count + "_V");
+            LPAResultClustering.main(name_node + tmp_output_path + tmp_count, name_node + tmp_output_path + tmp_count + "_V");
             repeat_time--;
             tmp_count++;
         }
