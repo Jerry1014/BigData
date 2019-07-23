1f6c4383375a38d6d720e46a183c49c2d055499b
Jerry <757320383@qq.com>
2019-07-23 14:55:16 +0800

    完成文件上传功能

--- src/main/java/cn/neu/hadoop/bigdata/web/WebController.java
+++ src/main/java/cn/neu/hadoop/bigdata/web/WebController.java
@@ -1,36 +1,46 @@
 package cn.neu.hadoop.bigdata.web;
 
 import cn.neu.hadoop.bigdata.HadoopTemplate;
+import com.alibaba.fastjson.JSONObject;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.hadoop.fs.FileStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class WebController {
     @Autowired
     private HadoopTemplate hadoopTemplate;
 
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index.html";
+    }
+
     @ResponseBody
     @RequestMapping(value = "/fs")
-    public String check(@RequestParam(name = "path") String path) {
+    public String get_json_from_hdfs(@RequestParam(name = "path") String path) {
         JsonObject response_json = new JsonObject();
         if (path != null) {
             try {
                 boolean if_dir = hadoopTemplate.existDir(path, false);
 
                 if (if_dir) {
-                    if(! hadoopTemplate.existsFile(path)) throw new Exception("文件不存在");
+                    if (!hadoopTemplate.existsFile(path)) throw new Exception("文件不存在");
                     JsonArray dir_filename_list = new JsonArray();
                     FileStatus[] test = hadoopTemplate.list(path);
                     for (FileStatus i : hadoopTemplate.list(path)) {
                         JsonObject tem = new JsonObject();
                         tem.addProperty("dir_or_file", i.isDirectory() ? "Dir" : "file");
-                        tem.addProperty("path", i.getPath().toString());
+                        String tem_path = i.getPath().toString();
+                        tem.addProperty("path", tem_path.substring(tem_path.indexOf("/", 7)));
                         dir_filename_list.add(tem);
                     }
                     response_json.add("content", dir_filename_list);
@@ -50,9 +60,14 @@ public class WebController {
         return response_json.toString();
     }
 
-    @RequestMapping(value = "/")
-    public String hello() {
-        return "index.html";
+    @ResponseBody
+    @RequestMapping(value = "/upload")
+    public String upload(@RequestBody JSONObject post_json) {
+        try {
+            hadoopTemplate.write(post_json.getString("filepath"), post_json.getString("content"));
+            return "文件上传成功";
+        } catch (Exception e) {
+            return "文件保存失败 " + e.toString();
+        }
     }
-
 }
