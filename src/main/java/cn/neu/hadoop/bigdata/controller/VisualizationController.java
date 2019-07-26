f6eed68634b52ca0fc99f4945eb101248370d43e
Jerry <757320383@qq.com>
2019-07-26 11:02:35 +0800

    命名更正

--- src/main/java/cn/neu/hadoop/bigdata/controller/VisualizationController.java
+++ src/main/java/cn/neu/hadoop/bigdata/controller/VisualizationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class Visualization {
+public class VisualizationController {
     @RequestMapping(value = "/visualizing")
     public String visualization(@RequestParam(name = "chart") String method) {
         return method.equals("WordCloud") ? "visualizing-wordcloud.html" : "visualizing.html";
