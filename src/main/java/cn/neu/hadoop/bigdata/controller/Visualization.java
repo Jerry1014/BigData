779b7fc9bb318c202d5b1d4ffcd9a8e32d747f16
Jerry <757320383@qq.com>
2019-07-26 10:54:31 +0800

    完成visualization controller的重构

---
+++ src/main/java/cn/neu/hadoop/bigdata/controller/Visualization.java
@@ -0,0 +1,13 @@
+package cn.neu.hadoop.bigdata.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Visualization {
+    @RequestMapping(value = "/visualizing")
+    public String visualization(@RequestParam(name = "chart") String method) {
+        return method.equals("WordCloud") ? "visualizing-wordcloud.html" : "visualizing.html";
+    }
+}
